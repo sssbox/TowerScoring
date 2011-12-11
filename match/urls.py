df96e8e9c04f2a849f43c7ac22d4cef30c18366a
Sam Solomon <sssbox@gmail.com>
2011-12-11 14:07:19 -0600

    [master] Add url for score_event.

--- match/urls.py
+++ match/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = patterns('match.views',
     url(r'^finished_scoring_center/$', 'finished_scoring_center', \
             name='match_finished_scoring_center'),
+    url(r'^score_event/$', 'score_event', name='match_score_event'),
 )
